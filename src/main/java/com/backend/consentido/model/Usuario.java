519e4fb497feb209cf8c965fdefe467cc8b06415
Michael Estiven Corrales Mendez <corralesmendezm@gmail.com>
2025-05-30 16:55:59 -0500

    Cambios en models

--- src/main/java/com/backend/consentido/model/Usuario.java
+++ src/main/java/com/backend/consentido/model/Usuario.java
@@ -1,4 +1,23 @@
 package com.backend.consentido.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_usuario;
+    private String contrasena;
+    private String nombre;
+
 }
+
+//-- Tabla Usuario
+//CREATE TABLE Usuario (
+//        id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+//        contrasena VARCHAR(100),
+//nombre VARCHAR(100),
+//rol ENUM('Administrador', 'Usuario') NOT NULL
+//)ENGINE=InnoDB;
