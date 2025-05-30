8fc8d6976581c3ff1d9337093cc547dec329f8bb
Michael Estiven Corrales Mendez <corralesmendezm@gmail.com>
2025-05-30 15:40:05 -0500

    Initializr Maven dependencias

---
+++ src/main/java/com/backend/consentido/ConsentidoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.consentido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsentidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsentidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/consentido/ConsentidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.consentido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsentidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
